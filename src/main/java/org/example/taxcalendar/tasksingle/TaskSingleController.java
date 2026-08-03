fdf8831932bc3c4a167f3a16c7227f433c891f8f
tycja <tycja1806@gmail.com>
2026-08-03 17:40:59 +0200

    Add editTaskSingle endpoint to TaskSingleController and implement update logic in TaskSingleService

--- src/main/java/org/example/taxcalendar/tasksingle/TaskSingleController.java
+++ src/main/java/org/example/taxcalendar/tasksingle/TaskSingleController.java
@@ -1,10 +1,13 @@
 package org.example.taxcalendar.tasksingle;
 
+import jakarta.validation.Valid;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.example.taxcalendar.tasksingle.dto.TaskSingleResponse;
+import org.example.taxcalendar.tasksingle.dto.TaskSingleUpdate;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -29,5 +32,10 @@ public class TaskSingleController {
     return taskSingleService.getTasksSingleResponsesAll();
   }
 
-
+  @PatchMapping("/{id}")
+  @ResponseStatus(HttpStatus.OK)
+  public TaskSingleResponse editTaskSingle(@PathVariable Long id,
+      @Valid TaskSingleUpdate taskSingleUpdate) {
+    return taskSingleService.editTaskSingle(id, taskSingleUpdate);
+  }
 }
--- src/main/java/org/example/taxcalendar/tasksingle/TaskSingleService.java
+++ src/main/java/org/example/taxcalendar/tasksingle/TaskSingleService.java
@@ -1,15 +1,19 @@
 package org.example.taxcalendar.tasksingle;
 
 
+import java.time.LocalDate;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.example.taxcalendar.parenttasks.ParentTaskService;
 import org.example.taxcalendar.tasksingle.dto.TaskSingleResponse;
+import org.example.taxcalendar.tasksingle.dto.TaskSingleUpdate;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class TaskSingleService {
 
   private final ApplicationEventPublisher applicationEventPublisher;
@@ -36,5 +40,25 @@ public class TaskSingleService {
     List<TaskSingle> taskSingles = taskSingleRepository.findAll();
     return taskSingles.stream().map(TaskSingleService::changeTaskSingeToTaskSingleResponse)
         .toList();
+  }
+  @Transactional
+  public TaskSingleResponse editTaskSingle(Long id, TaskSingleUpdate taskSingleUpdate) {
+    TaskSingle taskToUpdate = taskSingleRepository.findById(id)
+        .orElseThrow(() -> new IllegalArgumentException(generateExceptionMessage(id)));
+
+    String name = taskSingleUpdate.nameTask();
+    if (name != null) {
+      taskToUpdate.setNameTask(name);
+    }
+    LocalDate deadline = taskSingleUpdate.deadline();
+    if (deadline != null) {
+      taskToUpdate.setDeadline(deadline);
+    }
+    LocalDate dateCompletion = taskSingleUpdate.dateCompletion();
+    if (dateCompletion != null) {
+      taskToUpdate.setDateCompletion(dateCompletion);
+    }
+    taskToUpdate = taskSingleRepository.save(taskToUpdate);
+    return changeTaskSingeToTaskSingleResponse(taskToUpdate);
   }
 }
